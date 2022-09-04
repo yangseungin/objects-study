91fde878e6f7bf2f262b17a16127e6c570a5be54
yangseung-in <rhfpdk92@naver.com>
2022-09-04 23:23:24 +0900

    refactor: 소극장이 판매원에게 판매로직 위임 - Theater에서 TicketOffice의존성 제거

--- src/main/java/_01_ticket/TicketSeller.java
+++ src/main/java/_01_ticket/TicketSeller.java
@@ -2,11 +2,23 @@ package _01_ticket;
 
 import lombok.Getter;
 
-@Getter
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
+
+    public void sellTo(Audience audience){
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+
 }
