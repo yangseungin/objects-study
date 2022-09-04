99ec9d53ae3be2e8e6ad3a8368dc1260eefb3d17
yangseung-in <rhfpdk92@naver.com>
2022-09-04 22:01:06 +0900

    티켓 판매원 구현 - 티켓 판매원은 자신이 일하는 매표소를 알고 있어야함

---
+++ src/main/java/_01_ticket/TicketSeller.java
@@ -0,0 +1,12 @@
+package _01_ticket;
+
+import lombok.Getter;
+
+@Getter
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+}
