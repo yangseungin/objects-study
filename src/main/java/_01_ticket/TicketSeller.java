615427b652f7491a439932762ddb6e4a628eb1d3
yangseung-in <rhfpdk92@naver.com>
2022-09-04 23:59:06 +0900

    refactor: 관람객에게 티켓구매 위임 - 초대장 소지여부를 관람객 본인이 하도록 수정 - 본인의 가방에 본인이 접근하도록 수정

--- src/main/java/_01_ticket/TicketSeller.java
+++ src/main/java/_01_ticket/TicketSeller.java
@@ -1,7 +1,5 @@
 package _01_ticket;
 
-import lombok.Getter;
-
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
@@ -9,16 +7,8 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public void sellTo(Audience audience){
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 
 }
