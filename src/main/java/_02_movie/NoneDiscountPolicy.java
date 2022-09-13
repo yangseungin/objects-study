d0342f09e55b7e983bafbc793827963641ced4be
yangseung-in <rhfpdk92@naver.com>
2022-09-13 21:45:21 +0900

    할인없는 정책 추가

---
+++ src/main/java/_02_movie/NoneDiscountPolicy.java
@@ -0,0 +1,8 @@
+package _02_movie;
+
+public class NoneDiscountPolicy extends DiscountPolicy{
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
